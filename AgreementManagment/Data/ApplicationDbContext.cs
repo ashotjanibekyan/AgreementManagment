973dd348e872d1cc6b6798182ab81076aba1670e
Ashot Janibekyan <ashot.janibekyan@helpsystems.com>
2022-06-19 19:19:47 +0400

    Adding seed to make testing easier.

--- AgreementManagment/Data/ApplicationDbContext.cs
+++ AgreementManagment/Data/ApplicationDbContext.cs
@@ -17,10 +17,74 @@ namespace AgreementManagment.Data
             {
                 entity.HasIndex(e => e.Code).IsUnique();
             });
+            builder.Entity<Group>().HasData(
+                new Group
+                {
+                    Id = 1,
+                    Active = true,
+                    Code = "this is group code #1",
+                    Description = "this is group description #1",
+                },
+                new Group
+                {
+                    Id = 2,
+                    Active = false,
+                    Code = "this is group code #2",
+                    Description = "this is group description #2"
+                },
+                new Group
+                {
+                    Id = 3,
+                    Active = false,
+                    Code = "this is group code #3",
+                    Description = "this is group description #3"
+                },
+                new Group
+                {
+                    Id = 4,
+                    Active = false,
+                    Code = "this is group code #4",
+                    Description = "this is group description #4"
+                });
             builder.Entity<Product>(entity =>
             {
                 entity.HasIndex(e => e.Number).IsUnique();
             });
+
+            builder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Active = true,
+                    Description = "This is product description #1",
+                    Number = 42,
+                    Price = 42
+                },
+                new Product
+                {
+                    Id = 2,
+                    Active = true,
+                    Description = "This is product description #2",
+                    Number = 43,
+                    Price = 423
+                },
+                new Product
+                {
+                    Id = 3,
+                    Active = true,
+                    Description = "This is product description #4",
+                    Number = 4233,
+                    Price = 4123
+                },
+                new Product
+                {
+                    Id = 4,
+                    Active = true,
+                    Description = "This is product description #5",
+                    Number = 4983,
+                    Price = 4223
+                });
+
             builder.Entity<Agreement>();
             base.OnModelCreating(builder);
         }
---
+++ AgreementManagment/Migrations/20220619151648_addSeed.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgreementManagment.Migrations
+{
+    public partial class addSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Groups",
+                columns: new[] { "Id", "Active", "Code", "Description" },
+                values: new object[,]
+                {
+                    { 1, true, "this is group code #1", "this is group description #1" },
+                    { 2, false, "this is group code #2", "this is group description #2" },
+                    { 3, false, "this is group code #3", "this is group description #3" },
+                    { 4, false, "this is group code #4", "this is group description #4" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Active", "Description", "Number", "Price" },
+                values: new object[,]
+                {
+                    { 1, true, "This is product description #1", 42, 42m },
+                    { 2, true, "This is product description #2", 43, 423m },
+                    { 3, true, "This is product description #4", 4233, 4123m },
+                    { 4, true, "This is product description #5", 4983, 4223m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Groups",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
